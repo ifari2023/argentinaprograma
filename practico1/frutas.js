2a983732b5151172c0665a0690b887af469efb61
ifari2023 <ifari2012@gmail.com>
2023-06-13 22:17:44 -0300

    ejercicio 5

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas=[];
+frutas.push('manzana','pera','frutilla','banana','naranja');
+ultima=frutas[frutas.length-1];
+console.log('La ultima fruta agregada fue: '+ultima);
+primera=frutas[0];
+console.log('La primera fruta agregada fue: '+primera);
