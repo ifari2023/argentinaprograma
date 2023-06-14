7c6db5c2c6bf6329348dfef49d8777e5aa607afd
ifari2023 <ifari2012@gmail.com>
2023-06-13 22:16:24 -0300

    ejercicio 3

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,9 @@
+let var1=1;
+let var2=3;
+let var3=13;
+let var4=2;
+let resultado1=var1+var2;
+let resultado2=var3-var4;
+let resultadoFinal=resultado1*resultado2;
+let esPar=resultadoFinal%2===0
+console.log('"Mis variables iniciales fueron: '+var1+', '+var2+', '+var3+' y '+var4+'. La respuesta a verificar si el resultado final es par es: '+esPar+'”');
